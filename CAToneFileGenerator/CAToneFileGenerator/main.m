abf3b8b20eed992bc2e9b4203e98a1fd8f60cf93
Michael Fewkes <mdfewkes@gmail.com>
2026-09-21 18:19:53 -0700

    CAToneFileGenerator Saw

--- CAToneFileGenerator/CAToneFileGenerator/main.m
+++ CAToneFileGenerator/CAToneFileGenerator/main.m
@@ -10,7 +10,8 @@
 
 #define SAMPLE_RATE 44100
 #define DURATION 5.0
-#define FILENAME_FORMAT @"%0.3f-square.aif"
+//#define FILENAME_FORMAT @"%0.3f-square.aif"
+#define FILENAME_FORMAT @"%0.3f-saw.aif"
 
 int main(int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
@@ -51,11 +52,15 @@ int main(int argc, const char * argv[]) {
     while (sampleCount < maxSampleCount) {
         for (int i = 0; i < wavelengthInSamples; i++) {
             SInt16 sample;
-            if (i < wavelengthInSamples/2) {
-                sample = CFSwapInt16HostToBig(SHRT_MAX);
-            } else {
-                sample = CFSwapInt16HostToBig(SHRT_MIN);
-            }
+//            // Square
+//            if (i < wavelengthInSamples/2) {
+//                sample = CFSwapInt16HostToBig(SHRT_MAX);
+//            } else {
+//                sample = CFSwapInt16HostToBig(SHRT_MIN);
+//            }
+            // Saw
+            sample = CFSwapInt16HostToBig (((i / wavelengthInSamples) * SHRT_MAX *2) - SHRT_MAX);
+            
             audioErr = AudioFileWriteBytes(audioFile, false, sampleCount*2, &bytesToWrite, &sample);
             assert(audioErr == noErr);
             sampleCount++;
